a36f978821229233244cca9076a105b925794e21
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2022-01-11 12:17:33 +0530

    Modified Scraper

--- Notice_Scraping/index.js
+++ Notice_Scraping/index.js
@@ -9,18 +9,35 @@ const puppeteer = require("puppeteer");
     const elements = document.getElementsByClassName("BlogList-item-title");
     return Array.from(elements).map((element) => element.innerText);
   });
-
+  const hrefs1 = await page.evaluate(
+    () => Array.from(
+      document.querySelectorAll('a[href]'),
+      a => a.getAttribute('href')
+    )
+  );
+  var JsonLinks = JSON.stringify(hrefs1);
+  //console.log(JsonLinks);
   var JsonNotice = JSON.stringify(notice);
   //console.log(JsonNotice);
 
-  fs.writeFile("test.json", JsonNotice, function (err) {
+  fs.writeFile("testNotice.json", JsonNotice, function (err) {
     if (err) {
       console.log(err);
     }
     else{
-        console.log("Sucessfully Exported")
+        console.log("Sucessfully Exported Notice")
     }
   });
+  
+  fs.writeFile("testLink.json", JsonLinks, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else{
+        console.log("Sucessfully Exported Links")
+    }
+  });
+
 
   await browser.close();
 })();
